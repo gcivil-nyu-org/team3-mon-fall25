381233d0f5f9468e5c35bf74550b12964a8ac19e
iridiumtao <ct3354@nyu.edu>
2025-10-09 16:48:54 -0400

    Made sure the implemtation align with the description

--- backend/apps/listings/views.py
+++ backend/apps/listings/views.py
@@ -1,27 +1,14 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from .models import Listing
-from .serializers import (
-    ListingCreateSerializer, 
-    ListingDetailSerializer, 
-    ListingUpdateSerializer, 
-    CompactListingSerializer
-)
+from .serializers import ListingCreateSerializer
 
 # Create your views here.
 
-class ListingViewSet(viewsets.ModelViewSet):
+class ListingViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     """
-    A viewset for viewing and editing listings.
+    A viewset for creating listings.
+    Exposes a POST endpoint to /api/listings/.
     """
     queryset = Listing.objects.all()
-    serializer_class = ListingDetailSerializer  # Default serializer
-
-    def get_serializer_class(self):
-        if self.action == 'create':
-            return ListingCreateSerializer
-        if self.action == 'list':
-            return CompactListingSerializer
-        if self.action in ['update', 'partial_update']:
-            return ListingUpdateSerializer
-        return self.serializer_class
+    serializer_class = ListingCreateSerializer
