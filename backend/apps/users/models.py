22b051bff0d80521e6a301bf85b1213bb36e86b8
Rohit Farfad <rohitfarfad@gmail.com>
2025-10-18 14:05:38 -0400

    Added functionality to delete a lising

--- backend/apps/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    netid = models.CharField(max_length=255)
