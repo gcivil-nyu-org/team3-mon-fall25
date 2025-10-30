7f1e11f468b3856edf44e5c22ea18416496ae5cd
Liam <herryson5896@gmail.com>
2025-10-30 02:31:48 -0400

    Adjust STATIC_URL

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [ BASE_DIR / "static" ]
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
