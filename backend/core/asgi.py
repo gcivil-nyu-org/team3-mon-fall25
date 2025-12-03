9ac793813e0ad674f46f7524518fa0e01fdf51c2
Rohit Farfad <rohitfarfad@gmail.com>
2025-12-03 14:59:50 -0500

    Further fix for deployment failure

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,18 +1,21 @@
 import os
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings_local")
 
+from django.core.asgi import get_asgi_application  # noqa: E402
+
+# This runs django.setup() and loads all apps BEFORE we touch channels_jwt.
 django_asgi_app = get_asgi_application()
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from core.channels_jwt import JWTAuthMiddlewareStack
-from apps.chat.routing import websocket_urlpatterns
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
+from core.channels_jwt import JWTAuthMiddlewareStack  # noqa: E402
+from apps.chat.routing import websocket_urlpatterns  # noqa: E402
 
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": JWTAuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        "websocket": JWTAuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        ),
     }
 )
